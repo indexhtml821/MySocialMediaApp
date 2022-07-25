5b982f5c17a0970e48122a23ef3c3cca56132f7a
indexhtml821 <isaacopine@gmail.com>
2022-07-25 14:22:45 -0600

    se corrige ruta de url en signup y se añade signin

--- core/views.py
+++ core/views.py
@@ -2,12 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.http import HttpResponse
-
-from core.models import Profile
+from . models import Profile
 
 # Create your views here.
 
-
+#index view 
 def index(request):
     return render(request, 'index.html')
 
@@ -33,16 +32,35 @@ def signup(request):
 
                 #log user in and redirect to settings page
                # user_login = auth.authenticate(username=username, password=password)
-                #auth.login(request, user_login)
+               # auth.login(request, user_login)
 
                 #create a Profile object for the new user
                 user_model = User.objects.get(username=username)
                 new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
                 new_profile.save()
-                return redirect('signup')
+                return redirect('signin')
         else:
             messages.info(request, 'Password Not Matching')
             return redirect('signup')
         
     else:
         return render(request, 'signup.html')
+
+
+def signin(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+       
+        user = auth.authenticate(username = username, password = password)
+      
+        if user is not None:
+            auth.login(request,user)
+            return redirect('/')
+        else:
+            messages.info(request,'Credentials ivalid')
+            return redirect('signin') 
+   
+    else:
+        return render(request,'signin.html')
