6259a64b900742a8ec36b1b40225d10c8c531911
indexhtml821 <isaacopine@gmail.com>
2022-07-25 15:54:29 -0600

    configuración del .views para el logout

--- core/views.py
+++ core/views.py
@@ -1,5 +1,8 @@
+import imp
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User, auth
+from django.contrib.auth.models import User,auth
+from django.contrib.auth.decorators import login_required
+
 from django.contrib import messages
 from django.http import HttpResponse
 from . models import Profile
@@ -7,6 +10,7 @@ from . models import Profile
 # Create your views here.
 
 #index view 
+@login_required(login_url='signin')
 def index(request):
     return render(request, 'index.html')
 
@@ -49,18 +53,26 @@ def signup(request):
 
 def signin(request):
 
+
+    
+    
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
-       
-        user = auth.authenticate(username = username, password = password)
-      
+
+        user = auth.authenticate(username=username, password=password)
+
         if user is not None:
-            auth.login(request,user)
+            auth.login(request, user)
             return redirect('/')
         else:
-            messages.info(request,'Credentials ivalid')
-            return redirect('signin') 
-   
+            messages.info(request, 'Credentials Invalid')
+            return redirect('signin')
+
     else:
-        return render(request,'signin.html')
+        return render(request, 'signin.html')
+
+@login_required(login_url='signin')
+def logout(request): 
+    auth.logout(request)
+    return redirect('signin')       
