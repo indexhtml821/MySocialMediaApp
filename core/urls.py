7eeec08e705a6b0e0b0fd139525bd87ba993857b
indexhtml821 <isaacopine@gmail.com>
2022-07-25 14:22:58 -0600

    se añade el path de signin

--- core/urls.py
+++ core/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('',views.index,name='index'),
-    
+    path('',views.index,name='index'), 
     path('signup', views.signup,name='signup'),
+    path('signin', views.signin,name='signin'),
 
     
     ]
