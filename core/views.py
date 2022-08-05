de423b8ad3037b9dd6be37f26b1f03e8964b7b2f
indexhtml821 <isaacopine@gmail.com>
2022-08-04 19:52:23 -0600

    index muestra pulicaciones, opcion de like

--- core/views.py
+++ core/views.py
@@ -1,10 +1,11 @@
+from turtle import pos
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User,auth
 from django.contrib.auth.decorators import login_required
 
 from django.contrib import messages
 from django.http import HttpResponse
-from . models import Profile,Post
+from . models import Profile,Post,LikePost
 
 # Create your views here.
 
@@ -13,7 +14,9 @@ from . models import Profile,Post
 def index(request):
     user_object = User.objects.get(username = request.user.username)
     user_profile = Profile.objects.get(user=user_object)
-    return render(request, 'index.html',{'user_profile': user_profile})
+    posts = Post.objects.all()
+
+    return render(request, 'index.html',{'user_profile': user_profile, 'posts':posts})
 
 
 def signup(request):
@@ -124,3 +127,25 @@ def upload(request):
         return redirect('/')
     else:
         return redirect('/')
+
+@login_required(login_url='signin')
+def like_post(request):
+     username = request.user.username
+     post_id = request.GET.get('post_id')
+
+     post = Post.objects.get(id = post_id)
+
+     like_filter = LikePost.objects.filter(post_id=post_id, username = username).first
+
+     if like_filter == None:
+        new_like = LikePost.objects.create(post_id = post_id , username = username)
+        new_like.save()
+        post.no_of_likes = post.no_of_likes+1
+        post.save()
+        return redirect('/')
+     else:
+        like_filter.delete()
+        post.no_of_likes =post.no_of_likes - 1
+        post.save()
+        return redirect('/')
+
