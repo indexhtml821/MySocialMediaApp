fbe93b32466c209804bf7e0346d348aead4c0015
indexhtml821 <isaacopine@gmail.com>
2022-08-04 19:51:20 -0600

    se añade el modelo de likePost

--- core/models.py
+++ core/models.py
@@ -9,25 +9,34 @@ User = get_user_model()
 
 # Create your models here.
 
+
 class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     id_user = models.IntegerField(null=True)
     bio = models.TextField(blank=True)
-    profileimg = models.ImageField(upload_to='profile_images', default='blank-person-icon.jpg')
+    profileimg = models.ImageField(
+        upload_to='profile_images', default='blank-person-icon.jpg')
     location = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
         return self.user.username
+
 
 class Post(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     user = models.CharField(max_length=100)
     image = models.ImageField(upload_to='post_images')
     caption = models.TextField()
-    created_at = models.DateTimeField(default= datetime.now)
-    num_likes =  models.IntegerField(default=0)
+    created_at = models.DateTimeField(default=datetime.now)
+    num_likes = models.IntegerField(default=0)
 
-    def __str__(self) :
+    def __str__(self):
         return self.user
 
 
+class LikePost(models.Model):
+    post_id = models.CharField(max_length=500)
+    username = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.username
